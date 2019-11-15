6d2aa5b9e4b449cbb99b0bcc3bfc60f39a648892
Francois Godin <francois.h.godin@gmail.com>
2019-11-15 12:21:30 +0100

    ADD Cannot save on empty message
    ADD Cannot save on empty message
    
    ADD Cannot save on empty message

--- src/main/java/com/scholanova/projectdb/services/MessageService.java
+++ src/main/java/com/scholanova/projectdb/services/MessageService.java
@@ -1,5 +1,6 @@
 package com.scholanova.projectdb.services;
 
+import com.scholanova.projectdb.exceptions.MessageCannotBeEmptyException;
 import com.scholanova.projectdb.models.Message;
 import com.scholanova.projectdb.repositories.MessageRepository;
 import org.springframework.stereotype.Service;
@@ -16,9 +17,18 @@ public class MessageService {
         this.messageRepository = messageRepository;
     }
 
-    public Message create(Message message) {
+    public Message create(Message message) throws MessageCannotBeEmptyException{
+
+        if (!messageIsValid(message)) {
+            throw new MessageCannotBeEmptyException();
+        }
         Integer newId = messageRepository.create(message);
         return messageRepository.getById(newId);
+    }
+
+    private boolean messageIsValid(Message message) {
+        return message.getContent() != null &&
+                message.getContent().trim().length() > 0;
     }
 
     public List<Message> listAll() {
